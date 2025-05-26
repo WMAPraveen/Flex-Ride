2bf0bc78c112b77c66d74ab497c87b4cb7f16326
Avishka Wijesinghe <avishkapraveen02@gmail.com>
2025-05-26 15:06:30 +0530

    update

---
+++ flex_ride/lib/widgets/booking_summary_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class BookingSummaryItem extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const BookingSummaryItem({
+    Key? key,
+    required this.label,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey.shade600,
+            ),
+          ),
+          Text(
+            value,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
