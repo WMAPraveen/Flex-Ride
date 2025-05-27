60d52189e707fb4cf838a8519f8d3b4927d76667
wrtdilshan <135825001+wrtdilshan@users.noreply.github.com>
2025-05-27 06:35:40 +0530

    update

--- flex_ride/lib/features/auth/screen/loadingscreen.dart
+++ flex_ride/lib/features/auth/screen/loadingscreen.dart
@@ -12,4 +12,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    
+    // Navigate to WelcomeScreen after 10 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const WelcomeScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
