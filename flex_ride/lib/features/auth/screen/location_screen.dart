ad03b3dc8c3d53a756186af35187efe27838f53c
wrtdilshan <135825001+wrtdilshan@users.noreply.github.com>
2025-05-27 06:40:49 +0530

    update

---
+++ flex_ride/lib/features/auth/screen/location_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flex_ride/services/get_loaction.dart';
+import 'package:flutter/material.dart';
+
+ // your location file
+
+class LocationScreen extends StatelessWidget {
+  const LocationScreen({super.key});
