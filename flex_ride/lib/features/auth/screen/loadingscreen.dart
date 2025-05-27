0910c0bd38807160c9f6105838d1d56d3c8e4651
wrtdilshan <135825001+wrtdilshan@users.noreply.github.com>
2025-05-27 06:35:01 +0530

    update

---
+++ flex_ride/lib/features/auth/screen/loadingscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './welcomescreen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
