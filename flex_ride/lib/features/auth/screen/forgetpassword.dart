1a5c5119fb7fc8b998e6fe8f2a3fb260065423c9
wrtdilshan <135825001+wrtdilshan@users.noreply.github.com>
2025-05-27 06:25:11 +0530

    update

--- flex_ride/lib/features/auth/screen/forgetpassword.dart
+++ flex_ride/lib/features/auth/screen/forgetpassword.dart
@@ -13,3 +13,29 @@ class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
   final _emailController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _sendPasswordResetEmail() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      try {
+        await FirebaseAuth.instance.sendPasswordResetEmail(
+          email: _emailController.text.trim(),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              'Password reset email sent',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.grey,
+          ),
+        );
+        
