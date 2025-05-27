0589b6ec9d7b1b1fb29db7846db647dea609d4b7
wrtdilshan <135825001+wrtdilshan@users.noreply.github.com>
2025-05-27 06:41:40 +0530

    update

--- flex_ride/lib/features/auth/screen/location_screen.dart
+++ flex_ride/lib/features/auth/screen/location_screen.dart
@@ -5,3 +5,20 @@ import 'package:flutter/material.dart';
 
 class LocationScreen extends StatelessWidget {
   const LocationScreen({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Location")),
+      body: FutureBuilder<String>(
+        future: GetLocationServices().getCityNameFromCurrentLocation(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text("Error getting location"));
+          }
+
+          
